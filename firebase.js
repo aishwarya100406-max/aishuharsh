41fc4e6da9859ff453471b9736713350c816cf30
aishwarya100406-max <aishwarya100406@gmail.com>
2025-09-06 16:22:13 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKc_DVWrgLSmlh5S3ds_siqTyyfwPQUZQ",
+  authDomain: "haah-chat.firebaseapp.com",
+  projectId: "haah-chat",
+  storageBucket: "haah-chat.firebasestorage.app",
+  messagingSenderId: "957323289220",
+  appId: "1:957323289220:web:c138efa2cd45343dc918d1"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
